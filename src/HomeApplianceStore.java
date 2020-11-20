859acc0a0d5c52da25de3500e3f62871a3500e06
Chriswp <nanmushroom@live.com>
2020-11-20 16:38:15 +0200

    #5

--- src/HomeApplianceStore.java
+++ src/HomeApplianceStore.java
@@ -1,24 +1,85 @@
-/**
- * PUBLIC CLASS WITH MAIN
- */
-public class HomeApplianceStore {
-    /**
-     * Main Fuction , We Create New Devices and we Insert Values to them.
-     * We Display their Details and in the End how many Devices of different types we have.
-     * @param args NO USE OF ARGS
-     */
-    public static void main(String[] args) {
 
-    Fridge Fridge = new Fridge("Ikea" , "Legan" , "Fridge" , 179 , 64 , 70 , 294 , "White" , 4 , 794 , 1 , 64);
-    Fridge Fridge2 = new Fridge("Ikea" , "Tinad" , "Fridge" , 179 , 92 , 72 , 310 , "Black" , 3 , 584 , 1 , 44);
-    WashingMachine Machine = new WashingMachine("Bosch" , "Wan282K8GR" , "WashingMachine" , 59 , 84, 59 , 289 , "White" , 1400 , 8 , 44 , 33);
-    Oven Oven = new Oven("Gamba" , "KH3T" , "Oven" , 48 , 33 , 31 , 120 , "Black" , 28 , 55 , 4 , 3);
-    AirCondition Air = new AirCondition("Toyosima" , "TRN/TRG-856ZR" , "AirCondition" , 955 , 700 , 396 , 700 , "White" , 17743 , 14330 ,59 , 64);
-    AirCondition Air2 = new AirCondition("Toyosima" , "TRN/TRG-100ZR" , "AirCondition" , 930 , 680 , 372 , 689 , "Black" , 17743 , 14330 ,60 , 34);
+interface DeviceBasic{
 
-        System.out.println(Fridge.FridgeCounter());
-        System.out.println(Machine.WashingMachineCounter());
-        System.out.println(Oven.OvenCounter());
-        System.out.println(Air.AirConditionCounter());
-    }
+    void PowerOn();
+    void PowerOff();
+    int PeriodicMantainance();
+    boolean Status();
+
+    // Signature
+    void SetDesigner(String Designer);
+    void SetDeviceName(String DeviceName);
+    void SetType(String Type);
+    void SetLength(int Length);
+    void SetHeight(int Height);
+    void SetDepth(int Depth);
+    void SetConsume(int Consume);
+    String GetDesigner();
+    String GetDeviceName();
+    String GetType();
+    int GetLength();
+    int GetHeight();
+    int GetDepth();
+    int GetConsume();
+
+    void DisplayDetails();
 }
+
+abstract class Device implements DeviceBasic{
+
+    // Common Variables in other Classes.
+    public static String Designer;
+    public static String DeviceName;
+    public static String Type;
+    public static  int Length;
+    public static  int Height;
+    public static int Depth;
+    public static int Consume;
+
+
+    // Common Methods Between Classes
+    // Basic Setters - Getters.
+    public abstract void SetDesigner(String Designer);
+    public abstract void SetDeviceName(String DeviceName);
+    public abstract void SetType(String Type);
+    public abstract void SetLength(int Length);
+    public abstract void SetHeight(int Height);
+    public abstract void SetDepth(int Depth);
+    public abstract void SetConsume(int Consume);
+    // Getters
+    public abstract String GetDesigner();
+    public abstract String GetDeviceName();
+    public abstract String GetType();
+    public abstract int GetLength();
+    public abstract int GetHeight();
+    public abstract int GetDepth();
+    public abstract int GetConsume();
+
+    // Controls Status of Device.
+    public static boolean status;
+
+
+    public abstract void DisplayDetails(); // Display Details of Device
+}
+
+
+public class HomeApplianceStore{
+    public static void main(String[] args) {
+        DeviceStatus();
+    }
+
+    public static void DeviceStatus(){
+
+        DeviceBasic Fridge = new Fridge("Bosch" , "HT/52" , "Fridge" , 223 , 332 , 234 ,523 );
+
+            if(Fridge instanceof DeviceBasic){
+                ((DeviceBasic)Fridge).PowerOn(); // Turning On Device.
+                System.out.printf("Device Status: "); // Display Status
+                if(((DeviceBasic)Fridge).Status()) System.out.printf("Online"); else System.out.printf("Offline");
+            }
+            else{
+                System.out.println("ERR: ..."); return;
+            }
+        }
+}
+
